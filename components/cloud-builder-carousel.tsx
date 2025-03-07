bef403458f9dbab1abe1d47600fcf6b06812444b
sleep-deepfried <pulidoearljohn14@gmail.com>
2025-03-08 01:54:45 +0800

    optimized the carousel

--- components/cloud-builder-carousel.tsx
+++ components/cloud-builder-carousel.tsx
@@ -9,25 +9,24 @@ export default function CloudBuilderCarousel({ items }: CustomCarouselProps) {
                 orientation="vertical"
                 className="w-full"
                 opts={{
-                dragFree: false,
-                slidesToScroll: 1,
-                loop: false,
-                duration: 15
+                    loop: false,
+                    duration: 1000 / 60,
+                    dragFree: false
                 }}
             >
-            <CarouselContent className="w-full h-[400px]">
-                {items.map((item, index) => (
-                    <CarouselItem key={index} className="w-full">
-                    <div className="relative flex justify-center items-center mt-10">
-                        <div className="bg-[#2CBA9F] w-3xs h-[307px] px-5 pt-7 rounded-4xl space-y-5">
-                        <p className="text-2xl text-[#FCF8B7] font-bold text-center">{item.title}</p>
-                        <p className="text-xs text-white text-center">{item.description}</p>
-                        </div>
-                        <Image src="/images/cloud.svg" alt="cloud" width={280} height={133} className="absolute -bottom-10" />
-                    </div>
-                    </CarouselItem>
-                ))}
-            </CarouselContent>
+                <CarouselContent className="w-full h-[400px]">
+                    {items.map((item, index) => (
+                        <CarouselItem key={index} className="w-full">
+                            <div className="relative flex justify-center items-center mt-10">
+                                <div className="bg-[#2CBA9F] w-3xs h-[307px] px-5 pt-7 rounded-4xl space-y-5">
+                                    <p className="text-2xl text-[#FCF8B7] font-bold text-center">{item.title}</p>
+                                    <p className="text-xs text-white text-center">{item.description}</p>
+                                </div>
+                                <Image src="/images/cloud.svg" alt="cloud" width={280} height={133} className="absolute -bottom-10" />
+                            </div>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
             </Carousel>
         </div>
     );
