5a0af05f863ae5a97cb11eb49bf656101d86139d
sleep-deepfried <pulidoearljohn14@gmail.com>
2025-03-08 02:04:30 +0800

    applied use memo

--- components/cloud-builder-carousel.tsx
+++ components/cloud-builder-carousel.tsx
@@ -1,8 +1,31 @@
 import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
 import Image from "next/image";
 import { CustomCarouselProps } from "@/types";
+import { useMemo } from "react";
 
 export default function CloudBuilderCarousel({ items }: CustomCarouselProps) {
+    const carouselItems = useMemo(() => {
+        return items.map((item, index) => (
+            <CarouselItem key={index} className="w-full">
+                <div className="relative flex flex-col justify-center items-center mt-10">
+                    <div className="bg-[#2CBA9F] w-3xs h-[307px] px-5 pt-7 rounded-4xl space-y-5">
+                        <p className="text-2xl text-[#FCF8B7] font-bold text-center">{item.title}</p>
+                        <p className="text-xs text-white text-center">{item.description}</p>
+                    </div>
+                    <Image 
+                        src="/images/cloud.svg" 
+                        alt="cloud" 
+                        width={280} 
+                        height={133} 
+                        className="absolute -bottom-10"
+                        loading="lazy"
+                        priority={false}
+                    />
+                </div>
+            </CarouselItem>
+        ));
+    }, [items]);
+
     return (
         <div className="w-full overflow-hidden">
             <Carousel
@@ -10,22 +33,10 @@ export default function CloudBuilderCarousel({ items }: CustomCarouselProps) {
                 className="w-full"
                 opts={{
                     loop: false,
-                    dragFree: false
+                    dragFree: false,
                 }}
             >
-                <CarouselContent className="w-full h-[400px]">
-                    {items.map((item, index) => (
-                        <CarouselItem key={index} className="w-full">
-                            <div className="relative flex justify-center items-center mt-10">
-                                <div className="bg-[#2CBA9F] w-3xs h-[307px] px-5 pt-7 rounded-4xl space-y-5">
-                                    <p className="text-2xl text-[#FCF8B7] font-bold text-center">{item.title}</p>
-                                    <p className="text-xs text-white text-center">{item.description}</p>
-                                </div>
-                                <Image src="/images/cloud.svg" alt="cloud" width={280} height={133} className="absolute -bottom-10" />
-                            </div>
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
+                <CarouselContent className="w-full flex flex-col h-[400px]">{carouselItems}</CarouselContent>
             </Carousel>
         </div>
     );
